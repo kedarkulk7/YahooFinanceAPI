05f7f59f9dc9cf5b3dbc419bded85027049952f3
Kedar Kulkarni <kedarkulk7@gmail.com>
2020-10-13 17:49:13 -0500

    Update

---
+++ src/main/java/yahoo/finance/api/Test.java
@@ -0,0 +1,20 @@
+package yahoo.finance.api;
+
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		YahooFinanceAPI yahooFinanceAPI = new YahooFinanceAPI();
+		JSONArray array = yahooFinanceAPI.getCandleData("APPL", "15m", "5d", null, null);
+		
+		for(int i = 0; i < array.length(); i++) {
+			JSONObject obj = array.getJSONObject(i);
+			System.out.println("Time: "+ new Date(obj.getLong("time"))+", open:"+ obj.getDouble("open")+", high: "+ 
+		obj.getDouble("high")+", low: "+obj.getDouble("low")+", close: "+ obj.getDouble("close")+", volume: "+ obj.getLong("volume"));
+		}
+	}
+}
--- src/main/java/yahoo/finance/api/YahooFinanceAPI.java
+++ src/main/java/yahoo/finance/api/YahooFinanceAPI.java
@@ -17,8 +17,8 @@ public class YahooFinanceAPI {
 	 * @param symbol Valid trading symbol of the stock as per yahoo finance website
 	 * @param interval Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
 	 * @param range Valid Ranges: [1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max]
-	 * @param fromDate
-	 * @param toDate
+	 * @param fromDate Start date
+	 * @param toDate End Date
 	 * @return JSONArray JSONArray containing candle data. Each jsonobject representing each candle and respective parameters can be fetched using "open", "high", "low", "close", "volume", "time" key
 	 */
 	public JSONArray getCandleData(String symbol, String interval, String range, Date fromDate, Date toDate) {
@@ -48,22 +48,25 @@ public class YahooFinanceAPI {
 					JSONArray lowArray = jobj.getJSONObject("chart").getJSONArray("result").getJSONObject(0).getJSONObject("indicators").getJSONArray("quote").getJSONObject(0).getJSONArray("low");
 					JSONArray closeArray = jobj.getJSONObject("chart").getJSONArray("result").getJSONObject(0).getJSONObject("indicators").getJSONArray("quote").getJSONObject(0).getJSONArray("close");
 					JSONArray volumeArray = jobj.getJSONObject("chart").getJSONArray("result").getJSONObject(0).getJSONObject("indicators").getJSONArray("quote").getJSONObject(0).getJSONArray("volume");
-					
-					for(int i = 0; i < timestampArray.length(); i++) {
-						JSONObject newObj = new JSONObject();
-						newObj.put("time", timestampArray.getLong(i)*1000);
-						newObj.put("open", openArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
-						newObj.put("high", highArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
-						newObj.put("low", lowArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
-						newObj.put("close", closeArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
-						newObj.put("volume", volumeArray.get(i));
-						jsonArray.put(newObj);
+					try {
+						for(int i = 0; i < timestampArray.length(); i++) {
+							JSONObject newObj = new JSONObject();
+							newObj.put("time", timestampArray.getLong(i)*1000);
+							newObj.put("open", openArray.isNull(i) ? 0.0 : openArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
+							newObj.put("high", highArray.isNull(i) ? 0.0 : highArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
+							newObj.put("low", lowArray.isNull(i) ? 0.0 : lowArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
+							newObj.put("close", closeArray.isNull(i) ? 0.0 : closeArray.getBigDecimal(i).setScale(2, RoundingMode.HALF_EVEN));
+							newObj.put("volume", volumeArray.isNull(i) ? 0.0 : volumeArray.get(i));
+							jsonArray.put(newObj);
+						}
+					} catch(Exception e) {
+						System.out.println("YahooFinanceAPIExceptionLoop: " + e);
 					}
 				}
 			}
 			con.disconnect();
 		} catch (Exception e) {			
-			System.out.println("YahooFinanceAPIException: "+ e);
+			System.out.println("YahooFinanceAPIExceptionLoop: " + e);
 		}
 		return jsonArray;
 	}
